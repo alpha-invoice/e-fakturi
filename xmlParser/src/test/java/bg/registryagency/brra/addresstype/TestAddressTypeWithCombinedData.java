26a4a06aca8b073046183ae5adcd657ea12b3604
Dimitar Pahnev <dimitar.pahnev@paysafe.com>
2016-09-14 15:30:40 +0300

    [INT-618]Added test for AddressTypeParser

--- xmlParser/src/test/java/bg/registryagency/brra/addresstype/TestAddressTypeWithCombinedData.java
+++ xmlParser/src/test/java/bg/registryagency/brra/addresstype/TestAddressTypeWithCombinedData.java
@@ -1,4 +1,4 @@
-package bg.registryagency.brra;
+package bg.registryagency.brra.addresstype;
 
 import java.util.Map;
 
@@ -10,15 +10,15 @@ import org.testng.annotations.Test;
 import bg.registryagency.BrraParser;
 import bg.registryagency.model.BrraCompany;
 
-@Test
-public class TestAddressTypeParser {
+public class TestAddressTypeWithCombinedData {
+
+    private static final String FILES_FOR_TEST_LOCATION = "file:///C:/Users/dimitarpahnev/workspace/Invoices/invoices-brra/invoices-remastered/xmlParser/src/main/resources/static/testxml";
 
     private Map<Long, BrraCompany> parsedCompanies;
 
-    @BeforeClass
-    private void setUp(){
-        BrraParser source = new BrraParser(
-                "file:///C:/Users/dimitarpahnev/workspace/Invoices/invoices-brra/invoices-remastered/xmlParser/src/main/resources/static/testxml/2008");
+    @BeforeClass()
+    private void setUp() {
+        BrraParser source = new BrraParser(FILES_FOR_TEST_LOCATION);
         try {
             parsedCompanies = source.parseAll();
         } catch (Exception e) {
@@ -27,20 +27,20 @@ public class TestAddressTypeParser {
         }
     }
 
-    @DataProvider(name = "address output for 2008")
-    public Object[][] provideCorrectAddressStrings() {
+    @DataProvider(name = "address output for combined XML")
+    public Object[][] addressOutputForCombinedXML() {
         return new Object[][] {
-                { 117683847L, "ул.ДАМЕ ГРУЕВ 3, гр. Русе, БЪЛГАРИЯ, 7015" },
+                { 117683847L, "ул.ДАМЕ ГРУЕВ НОВ, гр. Русе, БЪЛГАРИЯ" },
                 { 117694911L, "ул. ДАМЕ ГРУЕВ 3, гр. Русе, БЪЛГАРИЯ, 7015" },
                 { 104645992L, "ул.ШЕЙНОВО 28, гр. Горна Оряховица, БЪЛГАРИЯ, 5100" },
-                { 200233939L, "ул. Сезам 17, ж.к. СВЕТА ТРОИЦА, бл. 303Б, гр. София, БЪЛГАРИЯ, 1309" },
+                { 200233939L, "ж.к. СВЕТА ТРОИЦА, бл. 303Б, гр. София, БЪЛГАРИЯ, 1309" },
                 { 104103644L, "КИРИЛ Д.АВРАМОВ 32, гр. Свищов, БЪЛГАРИЯ, 5250" }
         };
     }
 
-    @Test(dataProvider = "address output for 2008")
-    public void testAddress(Long id, String address) {
-
+    @Test(dataProvider = "address output for combined XML")
+    public void testCombinedAddresses(Long id, String address) {
         Assert.assertEquals(parsedCompanies.get(id).getAddress(), address);
     }
+
 }
---
+++ xmlParser/src/test/java/bg/registryagency/brra/addresstype/TestAddressTypeWithIncorrectData.java
@@ -0,0 +1,45 @@
+package bg.registryagency.brra.addresstype;
+
+import java.util.Map;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import bg.registryagency.BrraParser;
+import bg.registryagency.model.BrraCompany;
+
+public class TestAddressTypeWithIncorrectData {
+
+    private static final String FILE_FOR_2010_LOCATION = "file:///C:/Users/dimitarpahnev/workspace/Invoices/invoices-brra/invoices-remastered/xmlParser/src/main/resources/static/testxml/2010";
+    private Map<Long, BrraCompany> parsedCompanies;
+
+    @BeforeClass()
+    private void setUp() {
+        BrraParser source = new BrraParser(
+                FILE_FOR_2010_LOCATION);
+        try {
+            parsedCompanies = source.parseAll();
+        } catch (Exception e) {
+            e.printStackTrace();
+            Assert.fail(e.getMessage() + System.lineSeparator() + "Test Failed due to exception in BrraParser.");
+        }
+    }
+
+    @DataProvider(name = "address output for 20100605")
+    public Object[][] addressOutputForIncorrectXML() {
+        return new Object[][] {
+                { 117683847L, "ул.ДАМЕ ГРУЕВ НОВ, гр. Русе, БЪЛГАРИЯ" },
+                { 117694911L, null },
+                { 104645992L, "ул.ШЕЙНОВО 28, гр. Горна Оряховица, БЪЛГАРИЯ, 5100" },
+                { 104103644L, null },
+                { 200233939L, "ж.к. СВЕТА ТРОИЦА, бл. 303Б, гр. София, БЪЛГАРИЯ, 1309" }
+        };
+    }
+
+    @Test(dataProvider = "address output for 20100605")
+    public void testSecondXMLAddresses(Long id, String address) {
+        Assert.assertEquals(parsedCompanies.get(id).getAddress(), address);
+    }
+}
